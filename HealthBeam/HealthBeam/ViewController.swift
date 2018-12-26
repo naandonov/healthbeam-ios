ec6214cbbbe95302fb226dae46a45cd8164ccef9
Nikolay Andonov <nikolay.andonov@mentormate.com>
2018-12-26 11:31:31 +0200

    Initial application commit

---
+++ HealthBeam/HealthBeam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthBeam
+//
+//  Created by Nikolay Andonov on 26.12.18.
+//  Copyright © 2018 nikolay.andonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
